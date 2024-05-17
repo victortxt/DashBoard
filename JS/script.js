0860aeb631df7ed8b3516b67cb8d347fbda59f11
Victor Fernandes Neves da Silva <108033088+victortxt@users.noreply.github.com>
2024-05-17 10:22:42 -0300

    commit

--- JS/script.js
+++ JS/script.js
@@ -17,7 +17,10 @@ function fadeToggle(element){
 
 btnSee.forEach((e)=> {
     e.addEventListener('click', (element) => {
-        console.log(element.target.parentNode.parentNode.parentNode)
+        console.log(element.target.parentNode.parentNode.parentNode.querySelector('.headerCard').querySelector('.componentHeader').querySelector('.title').textContent)
+        console.log(element.target.parentNode.parentNode.parentNode.querySelector('.headerCard').querySelector('.componentHeader').querySelector('.membros').textContent)
+        console.log(element.target.parentNode.parentNode.parentNode.querySelector('.imgCard').querySelector('.imgCardUrl').src)
+        console.log(element.target.parentNode.parentNode.parentNode.querySelector('.textCard').querySelector('.textAreaContent').textContent.trim())
     })
 
     fadeToggle(e)    
