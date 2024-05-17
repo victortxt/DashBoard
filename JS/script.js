bb9f544ec6146ef43dc145f01942dae44d9656fb
Victor Fernandes Neves da Silva <108033088+victortxt@users.noreply.github.com>
2024-05-17 09:26:31 -0300

    commit

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,28 @@
 const btnSee = document.querySelectorAll('.btnSee')
+const btnBack = document.querySelector('#backIN')
 const containerDash = document.querySelector('#squareMain')
-btnSee.forEach((e)=> {
-    e.addEventListener('click',() => {
-        alert('teste')
+const containerDocument = document.querySelector('#document')
+
+let contents = Array()
+
+
+
+
+function fadeToggle(element){
+    element.addEventListener('click',() => {
+        $(containerDash).fadeToggle('fast')
+        $(containerDocument).fadeToggle('fast')
     })
+}
+
+btnSee.forEach((e)=> {
+    e.addEventListener('click', (element) => {
+        console.log(element.target.parentNode.parentNode.parentNode)
+    })
+
+    fadeToggle(e)    
 })
+
+fadeToggle(btnBack)
 
     
