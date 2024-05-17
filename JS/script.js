def339d705c481e5319891acf99963074260551b
Victor Fernandes Neves da Silva <108033088+victortxt@users.noreply.github.com>
2024-05-17 08:38:24 -0300

    commit

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+const btnSee = document.querySelectorAll('.btnSee')
+const containerDash = document.querySelector('#squareMain')
+
+btnSee.forEach((e) => {
+    e.addEventListener('click',(element) => {
+        alert('teste')
+    })
+})
